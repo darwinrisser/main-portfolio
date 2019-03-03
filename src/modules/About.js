7a3be5481d8e0c9c7b053a9449e2bc5bd79fc526
Darwin Risser <riser.darwin@gmail.com>
2019-03-03 11:07:31 -0600

    updated about

--- src/modules/About.js
+++ src/modules/About.js
@@ -3,7 +3,21 @@ import './css/About.css'
 
 const About = (props) => {
   return(
-      <h1>About</h1>
+    <div className="about">
+      <div className="aboutBox">
+        {/* <div id="headIcon"><i class="fas fa-user-circle"></i></div> */}
+        <p id="aboutContent">I am a full-stack web developer from Austin, TX.</p>
+        <button id="resumeButton">RESUME <i class="fas fa-file-download"></i></button>
+        <p className="sectionHead">Front End</p>
+        <p className="items">React &#8226; Redux &#8226; Javascript &#8226; jQuery &#8226; HTML5 &#8226; CSS3 &#8226; SASS &#8226; Bootstrap</p>
+        <p className="sectionHead">Back End</p>
+        <p className="items">Node &#8226; Express &#8226; MongoDB &#8226; Mongoose &#8226; RESTful API &#8226; Fetch &#8226; Axios</p>
+        <p className="sectionHead">Tools</p>
+        <p className="items">Webpack &#8226; Git &#8226; Npm</p>
+        <p className="sectionHead">Software</p>
+        <p className="items">Adope Photoshop &#8226; Adobe Illustrator &#8226; Adobe After Effects</p>
+      </div>
+    </div>
   )
 }
 
--- src/modules/Main.js
+++ src/modules/Main.js
@@ -6,8 +6,8 @@ const Main = (props) => {
     <div className="main">
       <div className="container">
         <h1>Darwin Risser</h1>
-        <h2>FULL  STACK  DEVELOPER</h2>
-        <button>About</button><button>Work</button><button>Contact</button>
+        <h2>FULL-STACK DEVELOPER</h2>
+        <button><i class="fas fa-user-circle"></i> About</button><button><i class="fas fa-laptop-code"></i> Work</button><button><i class="fas fa-envelope"></i> Contact</button>
       </div>
     </div>
   )
