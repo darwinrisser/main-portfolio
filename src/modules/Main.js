9a71979b65e58ef50649b7a1d8a136fd55e72614
Darwin <risser.darwin@gmail.com>
2019-04-02 20:19:56 -0500

    resume

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -7,7 +7,8 @@ const Main = (props) => {
       <div className="container">
         <h1>Darwin Risser</h1>
         <h2>FRONT-END DEVELOPER</h2>
-        <button><i class="fas fa-file"></i> Resume</button>
+        <button><a href="https://darwinrisser.github.io/DarwinRisser.pdf" target="_blank" className=
+        "mainLink"><i class="fas fa-file"></i> Resume</a></button>
         <button onClick={()=>{
             props.changeState(1)}}><i class="fas fa-laptop-code"></i> Work</button>
         <button><a href="mailto: risser.darwin@gmail.com" target="_blank" className=
